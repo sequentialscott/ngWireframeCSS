f3c8c4f8e2d3cba476eaac41eb603cc67b7debb5
Scott McDaniel <Scott.McDaniel@finra.org>
2018-10-04 12:01:34 -0400

    Remaining breadcrumbs work

--- src/app/topics/breadcrumbs-topic/breadcrumbs-topic.component.ts
+++ src/app/topics/breadcrumbs-topic/breadcrumbs-topic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HighlightResult } from 'ngx-highlightjs';
 
 @Component({
   selector: 'sg-breadcrumbs-topic',
@@ -7,14 +8,40 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BreadcrumbsTopicComponent implements OnInit {
   @Input() topicListing: any;
+  highlightedCode: HighlightResult;
   linkset = [
     { label: 'Home', link: '#' },
     { label: 'First Level', link: '#' },
-    { label: 'Second Level', link: '#' }
+    { label: 'Second Level' },
+    { label: 'Current Page' }
   ];
+
+  code = `
+<sg-breadcrumb links="linkset"></sg-breadcrumb>
+
+<script>
+  // In the component using the breadcrumbs.
+  linkset = [
+    { label: 'Home', link: '#' },
+    { label: 'First Level', link: '#' },
+    { label: 'Second Level' },
+    { label: 'Current Page' }
+  ];
+</script>
+  `;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onHighlight(e) {
+    this.highlightedCode = {
+      language: e.language,
+      r: e.r,
+      second_best: '{...}',
+      top: '{...}',
+      value: '{...}'
+    };
+  }
 }
